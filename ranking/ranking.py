308bd7dc2bb600b87d1939c1a2d37cfa4a266721
gsam1 <gsamardjiev@gmail.com>
2017-12-28 13:30:16 +0200

    Scoring algo done. Next step - ranking all the articles

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -3,6 +3,11 @@ import json
 import numpy as np
 from datetime import datetime
 
+# HELPERS
+def new_dt_logger(published):
+    print 'New Datetime Fromat Detected: ' + published.encode()
+
+# Classes
 class DBHandler(object):
     '''
     Class for handling the DB connection.
@@ -18,7 +23,7 @@ class DBHandler(object):
             Update the article in the database database
         '''
         self.collection.update_one({'gid':gid},{'$set':{'score':score}}, upsert=False)
-        print 'Item updated'
+        # print 'Item updated: ' + gid
 
     def get_all(self):
         return self.collection.find()
@@ -32,8 +37,44 @@ def calc_individual_score(published, upvotes):
         Calculates the score on which the articles are going to be sorted
     '''
     pub = published.encode().split()
-    fmt = '%Y-%b-%d %H:%M:%S'
-    pub_date_time = pub[3] + '-' + pub[2] + '-' + pub[1] + ' ' + pub[4]
+
+    # There must be a smarter way of doing this.
+    if len(pub) == 6:
+        fmt = '%Y-%b-%d %H:%M:%S'
+        pub_date_time = pub[3] + '-' + pub[2] + '-' + pub[1] + ' ' + pub[4]
+    elif len(pub) == 1:
+        fmt = '%Y-%m-%d %H:%M:%S'
+        dt_split = pub[0].split('T')
+        date = dt_split[0]
+        time_split = dt_split[1].split('+')
+
+        if len(time_split) == 2:
+            time = time_split[0]
+
+        elif len(time_split) == 1:
+            time_ex_split = dt_split[1].split('.')
+
+            if len(time_ex_split) == 2:
+                time = time_ex_split[0]
+
+            elif len(time_ex_split) == 1:
+                if len(time_split[0].split('-')) == 2:
+                    time = time_split[0].split('-')[0]
+
+                elif len(time_split[0].split('-')) == 1:
+                    time = time_split[0].split('-')[0].rstrip('Z')
+
+                else:
+                    new_dt_logger(published)
+            else:
+                new_dt_logger(published)
+        else:
+            new_dt_logger(published)
+
+        pub_date_time = date + ' ' + time
+    else:
+        new_dt_logger(published)
+
     d1 = datetime.now()
     d2 = datetime.strptime(pub_date_time, fmt)
     time_diff = d1 - d2
@@ -51,16 +92,13 @@ def main():
     '''
         Executes the ranking algorithm
     '''
-    # Pull the data from the database
-    # Rank it based on 
+
     dbstring = json.loads(open('./config/config.json').read())['db']
     dbhandler = DBHandler(dbstring)
-    # 5a43e36b5c59736a447b1802
-    ar = dbhandler.get_one('5468752c2031342044656320323031372031353a32383a3234202b30303030')
-    calc_individual_score(ar['published'], 0)
-    
-
-
+    # pull all articles from the db and update their score
+    for article in dbhandler.get_all():
+        sc = calc_individual_score(article['published'], article['upvotes'])
+        dbhandler.update_article_score(article['gid'], sc)
 
 
 
